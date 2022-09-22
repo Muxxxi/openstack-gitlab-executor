d97ac23083799e16e6644d8873d0f5d85b105d18
istmxrein <maximilian.reinheimer@stud.h-da.de>
2022-09-22 15:05:40 +0200

    create unique names for private keys

--- cleanup.py
+++ cleanup.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 import openstack
-
 import env
+import os
 
 
 def main() -> None:
     conn = openstack.connect()
     for server in conn.compute.servers(name=env.VM_NAME):
         conn.compute.delete_server(server)
+    if os.path.exists(env.PRIVATE_KEY_PATH):
+        os.remove(env.PRIVATE_KEY_PATH)
     conn.delete_keypair(env.KEY_PAIR_NAME)
+
 
 if __name__ == "__main__":
     main()
--- env.py
+++ env.py
@@ -1,14 +1,13 @@
 import os
 
 VM_NAME = f"gitlab-builder-{os.getenv('CUSTOM_ENV_CI_RUNNER_ID')}-project-{os.getenv('CUSTOM_ENV_CI_PROJECT_ID')}-concurrent-{os.getenv('CUSTOM_ENV_CI_CONCURRENT_PROJECT_ID')}-job-{os.getenv('CUSTOM_ENV_CI_JOB_ID')}"  # noqa
-
 FLAVOR = os.getenv("CUSTOM_ENV_FLAVOR") or os.getenv("FLAVOR")
 BUILDER_IMAGE = os.getenv("CUSTOM_ENV_BUILDER_IMAGE") or os.getenv("BUILDER_IMAGE")
 NETWORK = os.getenv("CUSTOM_ENV_NETWORK") or os.getenv("NETWORK")
 KEY_PAIR_NAME = f'key-{VM_NAME}'
 SECURITY_GROUPS = os.getenv("CUSTOM_ENV_SECURITY_GROUPS") or os.getenv("SECURITY_GROUPS")
 USERNAME = os.getenv("CUSTOM_ENV_USERNAME") or os.getenv("USERNAME")
-PRIVATE_KEY_PATH = f"{os.getenv('HOME')}/priv_key"
+PRIVATE_KEY_PATH = f"{os.getenv('HOME')}/priv_key-{VM_NAME}"
 SSH_TIMEOUT = os.getenv("CUSTOM_ENV_SSH_TIMEOUT") or os.getenv("SSH_TIMEOUT") or "20"
 FLOATING_IP_NETWORK = os.getenv("FLOATING_IP_NETWORK") or "public"
 SSH_IP_VERSION = os.getenv("SSH_IP_VERSION") or "4"
